dc44388c88d7f87a66b60ddbdfd60695a0dcb51e
Huang Zisheng <759833174@qq.com>
2020-06-03 00:37:09 +0800

    binary_search

---
+++ binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(list1,item):
+    low = 0
+    high = len(list1) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list1[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+my_list = [1,3,5,7,9]
+print(binary_search(my_list,1))
